87f6b471a5f4b60057cf9cdf2178c96f722938ea
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-12-26 13:51:40 +0530

    signup-added-frontend

---
+++ public/signup/signup.js
@@ -0,0 +1,39 @@
+let btn = document.querySelector('#btn');
+let name1 = document.querySelector('#name');
+let email1 = document.querySelector('#email');
+let tel1 = document.querySelector('#tel');
+let pass1 = document.querySelector('#pass');
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let name=name1.value;
+    let email=email1.value;
+    let tel=tel1.value;
+    let pass=pass1.value;
+    let obj={
+        name:name,
+        email:email,
+        tel:tel,
+        pass:pass
+    }
+
+
+    axios.post('http://localhost:3000/sign-up', obj)
+        .then(response=>{
+            console.log('success');
+            console.log(response.data);
+            if(response.status===201){
+            alert('Successfuly signed up');
+         //   window.location.href='../login/login.html'
+            }
+            else{
+                alert('Something is wrong');
+            }
+        })
+        .catch(response => {
+            if(response.status===403)
+            {
+            alert('User already exists, Please Login');
+            }
+        });
+})
