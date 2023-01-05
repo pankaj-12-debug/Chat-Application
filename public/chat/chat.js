173d06c1c5a6e306b7b36a91628a96d6497bd3ad
pankaj-12-debug <kajopkkumar12@gmail.com>
2023-01-05 18:13:02 +0530

    group frontend add

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -1,4 +1,5 @@
 const chatMessage=document.getElementById('chatMessage');
+const availableGroup=document.getElementById('availableGroup')
 function  joinedNotification(userName)
 {
     chatMessage.innerHTML+=`<p><b>${userName} Joined</b></p>`
@@ -7,6 +8,9 @@ function showMessage(message,userName)
 {
     chatMessage.innerHTML+=`<p>${userName}: ${message}`
 }
+document.getElementById('addGroup').addEventListener('click',()=>{
+    document.getElementById("addGroupForm").classList.toggle("hidden")
+  });
 document.getElementsByClassName('btn')[0].addEventListener('click',()=>{
     let token=localStorage.getItem('token')
     let chatMessage=document.getElementById('chatMessageInput').value;
@@ -17,31 +21,61 @@ document.getElementsByClassName('btn')[0].addEventListener('click',()=>{
     .then(result=>{
       //  let userName=result.data.user.name;
         //showMessage(chatMessage,userName);
- window.location.reload()
+ //window.location.reload()
     })
     .catch(err=>{
         console.log('sendMessage is not working');
     })
 })
-  //  setInterval(()=>{ 
+//Add group 
+document.getElementById("addGroupSubmitButton").addEventListener('click',(e)=>{
+    e.preventDefault();
+    let token=localStorage.getItem('token')
+    let groupName = document.getElementById("inputGroupName").value;
+    let reqObj={
+      groupName
+    }
+    axios.post(`http://localhost:3000/addGroup`, reqObj,{headers: {Authorization: token,}})
+      .then((response) => {
+        //console.log(response);
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log('err 44 add group line');
+      });
+  });
 
-    //axios.get('http://localhost:3000/getAllMessage').then(response=>{
-      //  let message=response.data.result;
-       // chatMessage.innerHTML='';
-       // message.forEach(element => {
-         //   console.log(element);
-          // if(element.messageText=='JOINED'){
-           // joinedNotification(element.name)
-            //    }              
-                //else
-                //showMessage(element.messageText,element.name)
-       // });
-    //})
-    //.catch(err=>{
+  function viewGroupInUI(obj){
+    availableGroup.innerHTML += `<div><span>${obj.name}</span><button id="addUser" onclick='addUserGroup(event)'>Add User</button> <button onclick='openChat(event)'>Open Chat</button></div>`;
+  }
+  let groupName;
+  function addUserGroup(e){
+    groupName = e.target.parentNode.firstElementChild.innerText;
+    console.log('54',groupName)
+    document.getElementById("addUserForm").classList.toggle('hidden');
+  }
+  
+  window.addEventListener('DOMContentLoaded',()=>{
+    let token=localStorage.getItem('token')
+    axios
+      .get("http://localhost:3000/getAllGroup", {headers: {Authorization: token,}})
+      .then((result) => {
+        result.data.forEach((element) => {
+          viewGroupInUI(element);
+        });
+      })
+      .catch((err) => {
       //  console.log(err);
-       // console.log('get all message error');
-    //})
-//},1000);
+      });
+  })
+  
+
+
+  function openChat(e) {
+    groupName = e.target.parentNode.firstElementChild.innerText;
+    document.getElementById("chatHeader").innerHTML=`<h1>${groupName}</h1>`;
+  
+
 
 setInterval(() => {
     let messagesObj=JSON.parse(localStorage.getItem('messagesObj'))
@@ -54,17 +88,26 @@ setInterval(() => {
     else{
         lastMessageId=0;
     }
-    axios.get(`http://localhost:3000/getAllMessage?lastMessageId=${lastMessageId}`)
+    axios.get(`http://localhost:3000/getAllMessage?lastMessageId=${lastMessageId}&groupName=${groupName}`)
 .then(response=>{
     let message=response.data.result;
-    if(!localStorage.getItem('messagesObj')){
-        localStorage.setItem("messagesObj",JSON.stringify(message));
-      }else{
-        if(message.length>0){
-         messagesObj.push(message[0]);     
-         localStorage.setItem("messagesObj", JSON.stringify(messagesObj));
+    if (!localStorage.getItem(`${groupName}`)) {
+        localStorage.setItem(`${groupName}`, JSON.stringify(message));
+      } else {
+        if (message.length > 0) {
+          messagesObj.push(message[0]);
+          localStorage.setItem(`${groupName}`, JSON.stringify(messagesObj));
         }
       }
+
+   // if(!localStorage.getItem('messagesObj')){
+     //   localStorage.setItem("messagesObj",JSON.stringify(message));
+     // }else{
+      //  if(message.length>0){
+        // messagesObj.push(message[0]);     
+        // localStorage.setItem("messagesObj", JSON.stringify(messagesObj));
+       // }
+     // }
     chatMessage.innerHTML='';
     if(Array.isArray(messagesObj)){
         messagesObj.forEach((element) => {
@@ -79,3 +122,22 @@ setInterval(() => {
      });
 //})
 }, 1000);
+  }
+//Add User
+  document.getElementById("addUserSubmitButton").addEventListener('click',()=>{
+    const inputEmail = document.getElementById("inputEmail").value;
+    const reqObj = {
+      inputEmail,
+      groupName,
+    };
+    console.log(reqObj);
+    axios
+      .post("http://localhost:3000/addUserGroup", reqObj)
+      .then((result) => {
+        console.log(result);
+        alert(result.data.message)
+      })
+      .then((err) => {
+        console.log(err);
+      });
+ });
